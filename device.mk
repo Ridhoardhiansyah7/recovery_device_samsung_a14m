622b93449bd9a635ae80ad568cbdfb66a38b23df
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2026-05-07 18:44:51 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,11 +3,6 @@ LOCAL_PATH := device/samsung/a14m
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
@@ -21,17 +16,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-# Debug
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
-    update_engine_client
 
 # Hidl
 PRODUCT_ENFORCE_VINTF_MANIFEST := false
